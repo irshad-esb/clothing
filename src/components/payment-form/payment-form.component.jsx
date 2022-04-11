4d782471933d87740e4dc31ffbc808c4606985f9
irshad-esb <irshad.chauhan@esparkbizmail.com>
2022-04-11 15:45:01 +0530

    ok

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,27 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+// import './payment-form-styles'
+import { PaymentFormContainer, FormContainer, PaymentButton } from "./payment-form-styles";
+
+
+const PaymentForm = () => {
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment</h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted} > Pay Now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  )
+}
+
+export default PaymentForm
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import App from "./App";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+import { store, persistor } from "./store/store";
+
+import "./index.scss";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+
+const rootElement = document.getElementById("root");
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
